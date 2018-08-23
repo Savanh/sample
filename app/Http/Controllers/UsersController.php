1b810bdf01280e0a004af39bb38dcb0a7d40a632
Savanh <2217905486@qq.com>
2018-08-23 10:56:32 +0800

    finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
+
 	public function create(){
 		return view('users.create');
 	}
@@ -31,7 +33,7 @@ class UsersController extends Controller
 			 'email' =>$request->email,
 			 'password'=>bcrypt($request->password)
 		]);
-
+		Auth::login($user);
 		session()->flash('success','欢迎，你将在这里开启一段新的旅程~');
 		return redirect()->route('users.show',[$user]);
 	}
