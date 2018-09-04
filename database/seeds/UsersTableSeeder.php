a7111aec285f25512006d735f85ba09fc164f6a7
Savanh <2217905486@qq.com>
2018-09-04 11:16:03 +0800

    Finish user edit, update, index, and destroy actions

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->makeVisible(['password','remember_token'])->toArray());
+
+        $user = User::find(1);
+        $user->name="Leon";
+        $user->email = "Leon2016@sina.com";
+        $user->password = bcrypt('password');
+        $user->is_admin = true;
+        $user->save();
+    }
+
+    public function run_2(){
+        $users = factory(User::class)->times(10)->make();
+        User::insert($users->makeVisible(['password','remember_token'])->toArray());
+
+        $user = User::find(1);
+
+    }
+
+}
