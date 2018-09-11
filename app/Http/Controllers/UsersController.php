df351b6a07a5ea7d1b7602e84f083fe034e4bc69
Savanh <2217905486@qq.com>
2018-09-11 14:44:53 +0800

    Add user statuses

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -29,9 +29,12 @@ class UsersController extends Controller
 		return view('users.create');
 	}
 
-	public function show(User $user){
-
-		return view('users.show',compact('user'));
+	public function show(User $user)
+	{
+		$statuses = $user->statuses()
+				->orderBy('created_at', 'desc')
+				->paginate(20);
+		return view('users.show', compact('user', 'statuses'));
 	}
 
 	public function store(Request $request){
